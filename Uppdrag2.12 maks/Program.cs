985602cf6b3257ade188cb3a7323df98d8d30b4e
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-16 14:13:57 +0200

    Add project files.

---
+++ Uppdrag2.12 maks/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace uppdrag2._12
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur många dagar ska du hyra bilen?");
+            String strNr = Console.ReadLine();
+            int nr = Convert.ToInt32(strNr);
+            nr = nr - 1;
+            int tr = nr * 500 + 300;
+            Console.WriteLine("Hur många KM ska du köra?");
+            string strKm = Console.ReadLine();
+            int km = Convert.ToInt32(strKm);
+            km = km ;
+            Console.Write("Så här mycket kommer din bil att kosta ");
+            Console.WriteLine(tr + km); 
+            
+        }
+    }
+}
+
